f2b30a6abdb27556b3f11f35ed4b3ec5b5943ebc
barailiaisha6 <barailiaisha6@gmail.com>
2025-11-23 00:47:29 -0800

    first add

---
+++ Square.java
@@ -0,0 +1,12 @@
+package day_5;
+   
+public class Square {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            int square = i * i;
+            System.out.println("Square of " + i + " is: " + square);
+        }
+
+    }
+}
