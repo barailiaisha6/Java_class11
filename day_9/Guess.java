c87c8658b5a3c04815b68e9e9236ec4c1886484b
barailiaisha6 <barailiaisha6@gmail.com>
2025-12-16 02:59:05 -0800

    add

--- day_9/Guess.java
+++ day_9/Guess.java
@@ -1,8 +1,10 @@
 package day_9;
 
+import java.util.scanner;
 
 public class Guess{
     double number;
+    int count = 0;
 
     public Guess(double number){
         this.number = number;
@@ -14,8 +16,27 @@ public class Guess{
         {
             System.out.println("Enter the number you want to guess");
 
-        int num = input.nextInt()
+        int num = input.nextInt();
+
+         count++;
+         if(num>number){
+         System.out.println("The number is greater than my secret number");
         }
-        }   
+        else if(num<number){
+            System.out.println("The number is lesser than my secret number");
+        }
+        else{
+            System.out.println("Your guess is correct");
+            break;
+        }
+        }  
+
+        System.out.println("Your guess number is " +count+ "times");
     }
+
+    public static void main(String []args){
+    Guess guess = new Guess(30);
+    guess.guessNumber();
 }
+}
+
